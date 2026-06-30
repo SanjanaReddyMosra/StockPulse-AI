b8a6d16990d93f54b23a25e5309760eea2cda4d2
SanjanaReddyMosra <mosrasanjanareddy@gmail.com>
2026-06-30 14:52:35 +0530

    'm'

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import API from "../api/stockAPI";xx
+import API from "../api/stockAPI";
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
